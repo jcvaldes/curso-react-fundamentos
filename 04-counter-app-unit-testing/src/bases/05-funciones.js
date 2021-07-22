ae357f73e7ebbeab15804f63b64983b1c63fd345
Juan Carlos Valdés <idevkingos@gmail.com>
2021-07-22 09:31:41 -0300

    feat: test unitarios base

---
+++ 04-counter-app-unit-testing/src/bases/05-funciones.js
@@ -0,0 +1,40 @@
+
+// Funciones en JS
+// const saludar = function( nombre ) {
+//     return `Hola, ${ nombre }`;
+// }
+
+const saludar2 = ( nombre ) => {
+    return `Hola, ${ nombre }`;
+}
+
+const saludar3 = ( nombre ) => `Hola, ${ nombre }`;
+const saludar4 = () => `Hola Mundo`;
+
+// console.log( saludar('Goku') )
+
+console.log( saludar2('Vegeta') );
+console.log( saludar3('Goku') );
+console.log( saludar4() );
+
+// OJO con esto cuando retorno un objeto lo encierro en parentesis
+const getUser = () => ({
+    uid: 'ABC123',
+    username: 'El capo'
+});
+
+
+const user = getUser();
+console.log(user);
+
+// Tarea
+const getUsuarioActivo = ( nombre ) =>({
+    uid: 'ABC567',
+    username: nombre
+})
+
+const usuarioActivo = getUsuarioActivo('Fernando');
+console.log( usuarioActivo );
+
+
+
