02856f7bcba4915ae7ea7aef66507c22d7c17a2f
Juan Carlos Valdes <juancarlos.valdez@externos-cl.cencosud.com>
2021-09-09 10:00:00 -0300

    fix: controles dinamicos

---
+++ 06-dynamic-form/src/config/jumbo-ar.js
@@ -0,0 +1,14 @@
+const controls = [
+  {
+    label: "Username",
+    name: "name",
+    type: "test",
+    placeholder: "please enter your name",
+  },
+  {
+    label: "E-mail",
+    name: "email",
+    type: "email",
+    placeholder: "please enter your email",
+  },
+];
